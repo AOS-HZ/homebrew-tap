2b309ae1cc65f2fd701aba816379d259d9e472dd
yueyong <yueyong1030@outlook.com>
2026-03-28 17:33:45 +0800

    Clear quarantine after cask install

--- Casks/aos.rb
+++ Casks/aos.rb
@@ -12,4 +12,10 @@ cask "aos" do
   depends_on macos: ">= :monterey"
 
   binary "aos-#{version}-darwin-arm64/aos"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-d", "com.apple.quarantine", "#{staged_path}/aos-#{version}-darwin-arm64/aos"],
+                   sudo: false
+  end
 end
